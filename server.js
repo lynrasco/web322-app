a5418377c5f294735c5fde6e2df4aa30bd9d9e3b
Lyndsey R <lrasco@myseneca.ca>
2024-10-09 15:29:01 -0400

    Everything setup

---
+++ server.js
@@ -0,0 +1,81 @@
+/*********************************************************************************
+
+WEB322 – Assignment 02
+I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
+
+Name: Lyndsey Rasco 
+Student ID: 173670233
+Date: October 6, 2024
+Cyclic Web App URL: _______________________________________________________
+GitHub Repository URL: ______________________________________________________
+
+********************************************************************************/ 
+//REMEMBER TO DEPLOY TO REPLIT INSTEAD OF CYCLIC!!!!!!!!1
+const storeService = require('./store-service');
+const express = require('express');
+const path = require("path");
+const app = express();
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.redirect('/about');
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/about.html'));
+});
+
+/*
+app.listen(HTTP_PORT, () => {
+    console.log(`Express http server listening on port ${HTTP_PORT}`);
+})
+*/
+
+storeService.initialize().then(() => {
+    app.listen(HTTP_PORT, () => {
+        console.log(`Express http server listening on port ${HTTP_PORT}`);
+    });
+}).catch(err => {
+    console.log(`Error initializing data: ` + err);
+});
+
+app.get('/items', (req, res) => {
+    //res.json(storeService.getAllItems());
+    storeService.getAllItems()
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err });
+        });
+});
+
+app.get('/categories', (req, res) => {
+    //res.json(storeService.getAllCategories());
+    storeService.getAllCategories()
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err });
+        });
+});
+
+app.get('/shop', (req, res) => {
+    //const publishedItems = storeService.getAllItems().filter(item => item.published === true);
+    //res.json(publishedItems);
+    storeService.getPublishedItems()
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err});
+        });
+});
+
+app.use((req, res) => {
+    res.status(404).send("404: Page Not Found");
+})
---
+++ store-service.js
@@ -0,0 +1,110 @@
+const fs = require('fs');
+const path = require('path');
+
+let items = [];
+let categories = [];
+
+/*
+function initialize() {
+    return new Promise((resolve, reject) => {
+        try {
+            items = JSON.parse(fs.readFileSync('./data/items.json', 'utf-8'));
+            categories = JSON.parse(fs.readFileSync('./data/categories.json', 'utf-8'));
+            resolve();
+        }
+        catch (err) {
+            reject("Unable to read data files: " + err);
+        }
+    });
+}
+*/
+
+function initialize() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path.join(__dirname, './data/items.json'), 'utf8', (err, data) => {
+            if (err) {
+                reject("Unable to read file items.json");
+                return;
+            }
+            try {
+                items =JSON.parse(data);
+            }
+            catch (err) {
+                reject("Error parsing items.json data");
+                return;
+            }
+
+            fs.readFile(path.join(__dirname, './data/categories.json'), 'utf8', (err, data) => {
+                if (err) {
+                    reject("Unable to read file categories.json");
+                    return;
+                }
+
+                try {
+                    categories = JSON.parse(data);
+                }
+                catch (err) {
+                    reject("Error parsing categories.json data");
+                    return;
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+//Old getAllItems() function
+/*
+function getAllItems() {
+    return items;
+}
+*/
+
+function getAllItems() {
+    return new Promise((resolve, reject) => {
+        if (items.length > 0) {
+            resolve(items);
+        }
+        else {
+            reject("No results returned");
+        }
+    });
+}
+
+function getPublishedItems() {
+    return new Promise((resolve, reject) => {
+        const publishedItems = items.filter(item => item.published === true);
+        if (publishedItems.length > 0) {
+            resolve(publishedItems);
+        }
+        else {
+            reject("No results returned");
+        }
+    });
+}
+
+//Old getAllCategories() function
+/*
+function getAllCategories() {
+    return categories;
+}
+*/
+
+function getAllCategories() {
+    return new Promise((resolve, reject) => {
+        if (categories.length > 0) {
+            resolve(categories);
+        }
+        else {
+            reject("No results returned");
+        }
+    });
+}
+
+module.exports = {
+    initialize,
+    getAllItems,
+    getPublishedItems,
+    getAllCategories
+    //getCategories
+};
